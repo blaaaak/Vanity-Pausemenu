5dd74dbe8d2e91d28586a8f527567a97d32c9861
Bill <103514165+blaaaak@users.noreply.github.com>
2024-07-11 15:41:36 +1000

    update(server) Localise a couple things.

--- server/server.lua
+++ server/server.lua
@@ -1,14 +1,18 @@
+local leaveReason = (Config.leaveReason ~= '' and Config.leaveReason) or 'Thank you for playing the server!'
+
 CreateThread(function()
     while true do
-        TriggerClientEvent("vanity-pause:currentPlayers", -1, GetNumPlayerIndices())
+        local currentPlayers = GetNumPlayerIndices()
+        TriggerClientEvent("vanity-pause:currentPlayers", -1, currentPlayers)
         Wait(30000)
     end
 end)
 
 RegisterNetEvent("vanity-pause:getMaxPlayers", function()
-    TriggerClientEvent('vanity-pause:maxPlayers', source, GetConvarInt('sv_maxclients', 64))
+    local maxPlayers = GetConvarInt('sv_maxclients', 64)
+    TriggerClientEvent('vanity-pause:maxPlayers', source, maxPlayers)
 end)
 
 RegisterNetEvent('vanity-pause:dropPlayer', function()
-    DropPlayer(source, (Config.leaveReason ~= '' and Config.leaveReason) or 'Thank you for playing the server!')
+    DropPlayer(source, leaveReason)
 end)
